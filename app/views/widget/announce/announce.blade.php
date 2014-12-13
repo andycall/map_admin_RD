5609d402ea6ef96aa9fd47efbbe51e1f6cfe61ff
unknown <492994082@qq.com>
2014-12-13 15:16:42 +0800

    update

--- app/views/widget/announce/announce.blade.php
+++ app/views/widget/announce/announce.blade.php
@@ -1,15 +1,14 @@
 <div id="announcement">
 
   <h3>餐厅公告</h3>
-  <div class="announcement">
-    <p>{{$data["announcement"]}}</p>
-  </div>
-  <textarea class="announcement"></textarea>
-  <h4 class="min_price">起送价格；
-    <span>{{$data["min_price"]}}</span>
-    <input type="text" class="min_price_input"/>
-  </h4>
+  <textarea class="announcement">{{$data["announcement"]}}</textarea>
+  <a class="btn announcement_save">保存</a>
 
+
+  <h4 class="min_price">起送价格；
+    <input type="text" class="min_price_input" value="{{$data["min_price"]}}"/>
+    <a class="btn min_price_save">保存</a>
+  </h4>
 
 
 </div>
