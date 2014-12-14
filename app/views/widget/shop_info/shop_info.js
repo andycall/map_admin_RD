126197de7bbc98595a9ba4ec2855e365ca6c44da
unknown <492994082@qq.com>
2014-12-14 23:05:22 +0800

    fix shop info js

--- app/views/widget/shop_info/shop_info.js
+++ app/views/widget/shop_info/shop_info.js
@@ -1,3 +1,14 @@
 define(['jquery', 'bootstrap'], function(){
+	$('#shopInfoForm').on('submit', checker);
+	function checker(){
+		var value = $('.shop_name_input').val();
+		if(!value){
+			alert('请填写商家名称.');
+			$('.shop_name_input').parents('.form-group').addClass('has-error');
+			return false;
+		}
+
+		return true;
+	}
 	console.log("shop info  loaded");
 });
